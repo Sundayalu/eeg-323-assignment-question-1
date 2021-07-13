a79198d0e4011a6ed97f7224c96ff6fa22c07d1b
Sundayalu <87358657+Sundayalu@users.noreply.github.com>
2021-07-13 08:04:59 +0100

    this is the answer to question 1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Assignment_Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+          //alu sunday ayo
+          // ees/19/20/0155
+            // This is Question 1
+            string student;
+            Console.Write(" are you are full-time or a part-time student:_"); 
+            student = Console.ReadLine();
+            string course;
+            int credit;
+
+            if(student == "fulltime"){
+               Console.WriteLine("Whats your course of study:_");
+              course = Console.ReadLine();
+              Console.WriteLine(course + "  Thats a Good Course !");   
+            }
+            else if(student == "partime"){
+                Console.WriteLine("How Many Credits Are You Taking:_");
+                 credit  = Convert.ToInt32(Console.ReadLine());
+                 if(credit >= 6){
+                     Console.WriteLine("Too Much Course");
+                 }
+                 else if(credit < 6){
+                     Console.WriteLine("Proceed to the screen");   
+                 }
+                
+            }
+           
+            
+        }
+    }
+}
